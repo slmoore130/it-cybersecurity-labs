95b8a027ffe940d08e5ead19761fff18fff0ce7b
slmoore130 <stevenlamar.moore@gmail.com>
2026-09-25 00:28:10 -0700

    Add initial Python system information tool

---
+++ python/system-information-tool/system_info.py
@@ -0,0 +1,8 @@
+import platform
+
+print("SYSTEM INFORMATION")
+print("------------------")
+print("Operating System:", platform.system())
+print("Computer Name:", platform.node())
+print("Processor:", platform.processor())
+print("Python Version:", platform.python_version())
